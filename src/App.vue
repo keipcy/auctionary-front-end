a03893d9eada872cf20852ea22caa39d57f5f2ad
Kothar Aljanabi <koaaljanabi@gmail.com>
2025-12-11 14:47:15 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ welcome_message }}</h1>
+    <p>Counter: {{ counter }}</p>
+    <button v-on:click="increment">Add one</button>
+
+    <p v-if="counter >= 5">Big numer</p>
+    <p v-else>Little number</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        welcome_message: "Hello World!",
+        counter: 0
+      }
+    },
+    methods: {
+      increment(){
+        this.counter ++;
+      }
+    }
+  }
+</script>
+
+<style scoped></style>
